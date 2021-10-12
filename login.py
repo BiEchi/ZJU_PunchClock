40fc736b40ed9718d30c1e1e267f2a37e1808578
Jackgetup <Haob.19@intl.zju.edu.cn>
2021-10-12 13:33:45 +0800

    changed to headless mode and finished the release of mac machines

--- login.py
+++ login.py
@@ -1,6 +1,6 @@
-from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import configparser
+from sys import exit
 
 
 class login:
@@ -9,40 +9,57 @@ class login:
         self.driver = driver
         self.account = "example.19@intl.zju.edu.cn"
         self.password = "PasswordYouLike"
+        self.timeout = 8
 
     def load_account_info(self):
         # load the config file
         config = configparser.ConfigParser()
         config.read("config.ini")
-        self.account = config['identification_information']['account_config']
-        self.password = config['identification_information']['password_config']
+        # Use scratch
+        # self.account = config['identification_information']['account_config']
+        # self.password = config['identification_information']['password_config']
+        # Use app
+        self.account = input("Please enter your account (INTL E-mail) e.g. Haob.88@intl.zju.edu.cn\n> ")
+        print("\033[0;32m%s\033[0m" % 'Your account is {}.'.format(self.account)) # 32m is green
+        self.password = input("Please enter your passwd e.g. Popu#7852\n> ")
+        print("\033[0;32m%s\033[0m" % 'Successfully typed in your Password.')  # 32m is green
         return
 
     def logging_in_process(self):
-        # click the "INTL ID" button
-        self.driver.implicitly_wait(10)
-        INTL_ID_button_second = self.driver.find_element_by_xpath(
-            '/html/body/div/div[3]/div[2]/div/div[2]/div/div[1]/div/div[1]/a/img')
-        INTL_ID_button_second.click()
+        print("\033[0;33m%s\033[0m" % 'Logging you in...')  # 33m is yellow
 
-        # enter the account name
-        self.driver.implicitly_wait(10)
-        account_input_box = self.driver.find_element_by_xpath(
-            '/html/body/div/form[1]/div/div/div[2]/div[1]/div/div/div/div/div/div[3]/div/div/div/div[2]/div[2]/div/input[1]')
-        account_input_box.send_keys(self.account)
-        account_input_box.send_keys(Keys.ENTER)
+        try:
+            # click the "INTL ID" button
+            self.driver.implicitly_wait(self.timeout)
+            INTL_ID_button_second = self.driver.find_element_by_xpath(
+                '/html/body/div/div[3]/div[2]/div/div[2]/div/div[1]/div/div[1]/a/img')
+            INTL_ID_button_second.click()
 
-        # enter the password and confirm
-        self.driver.implicitly_wait(10)
-        password_input_box = self.driver.find_element_by_xpath(
-            '/html/body/div/form[1]/div/div/div[2]/div[1]/div/div/div/div/div/div[3]/div/div[2]/div/div[2]/div/div[2]/input')
-        password_input_box.send_keys(self.password)
-        password_input_box.send_keys(Keys.ENTER)
 
-        # click the YES button
-        self.driver.implicitly_wait(10)
-        YES_button = self.driver.find_element_by_xpath('//*[@id="idSIButton9"]')
-        YES_button.click()
+            # enter the account name
+            self.driver.implicitly_wait(self.timeout)
+            account_input_box = self.driver.find_element_by_xpath(
+                '/html/body/div/form[1]/div/div/div[2]/div[1]/div/div/div/div/div/div[3]/div/div/div/div[2]/div[2]/div/input[1]')
+            account_input_box.send_keys(self.account)
+            account_input_box.send_keys(Keys.ENTER)
+
+            # enter the password and confirm
+
+            self.driver.implicitly_wait(self.timeout)
+            password_input_box = self.driver.find_element_by_xpath(
+                '/html/body/div/form[1]/div/div/div[2]/div[1]/div/div/div/div/div/div[3]/div/div[2]/div/div[2]/div/div[2]/input')
+            password_input_box.send_keys(self.password)
+            password_input_box.send_keys(Keys.ENTER)
+
+            # click the YES button
+            self.driver.implicitly_wait(self.timeout)
+            YES_button = self.driver.find_element_by_xpath('//*[@id="idSIButton9"]')
+            YES_button.click()
+
+        except:
+            print("\033[0;31m%s\033[0m" % 'Wrong account or password!')  # 31m is red
+            self.driver.close()
+            exit(0)
 
         return
 
--- main.py
+++ main.py
@@ -8,85 +8,107 @@ chrome_options = Options()
 chrome_options.add_argument('--headless')
 
 # use Chrome to access the clock-punching website
-driver = webdriver.Chrome(options=None)  # omit options if don't want to use headless mode; add options=chrome_options if you want to use headless mode
+driver = webdriver.Chrome(options=chrome_options)  # omit options if don't want to use headless mode; add options=chrome_options if you want to use headless mode
 driver.get('https://healthreport.zju.edu.cn/ncov/wap/default/index')
+print("\033[0;32m%s\033[0m" % '[10%] Successfully started the automata.')  # 32m is green
 
 # call the submodule login
 login_object = login(driver)
 login_object.login_caller()
+print("\033[0;32m%s\033[0m" % '[20%] Successfully logged you in.')  # 32m is green
 
 # fill in the form
 driver.implicitly_wait(10)
 
-jiezhong_button = driver.find_element_by_xpath\
+jiezhong_button = driver.find_element_by_xpath \
     ('/html/body/div[1]/div[1]/div/section/div[4]/ul/li[4]/div/div/div[1]/span[1]/i')
 jiezhong_button.click()
 sleep(1)
+print("\033[0;33m%s\033[0m" % '[30%] Automata processing to Vaccine part...')  # 33m is yellow
 
-buyi_button = driver.find_element_by_xpath\
+buyi_button = driver.find_element_by_xpath \
     ('/html/body/div[1]/div[1]/div/section/div[4]/ul/li[5]/div/div/div[5]/span[1]/i')
 buyi_button.click()
 sleep(1)
+print("\033[0;33m%s\033[0m" % '[36%] Automata processing to BY part...')  # 33m is yellow
 
-yiwancheng_button = driver.find_element_by_xpath\
+yiwancheng_button = driver.find_element_by_xpath \
     ('/html/body/div[1]/div[1]/div/section/div[4]/ul/li[6]/div/div/div[2]/span[1]/i')
 yiwancheng_button.click()
 sleep(1)
+print("\033[0;33m%s\033[0m" % '[40%] Automata processing to YWC part...')  # 33m is yellow
 
-fareqingjia_button = driver.find_element_by_xpath\
+fareqingjia_button = driver.find_element_by_xpath \
     ('/html/body/div[1]/div[1]/div/section/div[4]/ul/li[7]/div/div/div[2]/span[1]/i')
 fareqingjia_button.click()
 sleep(1)
+print("\033[0;33m%s\033[0m" % '[44%] Automata processing to Fever part...')  # 33m is yellow
 
-farewai_button = driver.find_element_by_xpath\
+farewai_button = driver.find_element_by_xpath \
     ('/html/body/div[1]/div[1]/div/section/div[4]/ul/li[8]/div/div/div[2]/span[1]/i')
 farewai_button.click()
 sleep(1)
+print("\033[0;33m%s\033[0m" % '[47%] Automata processing to FW part...')  # 33m is yellow
 
-jiankangma_button = driver.find_element_by_xpath\
+jiankangma_button = driver.find_element_by_xpath \
     ('/html/body/div[1]/div[1]/div/section/div[4]/ul/li[13]/div/div/div[1]/span[1]/i')
 jiankangma_button.click()
 sleep(1)
+print("\033[0;33m%s\033[0m" % '[52%] Automata processing to Healthcare Code part...')  # 33m is yellow
 
 # will show up after clicking "jiankangma" button
-jiankangmayanse_button = driver.find_element_by_xpath\
+jiankangmayanse_button = driver.find_element_by_xpath \
     ('/html/body/div[1]/div[1]/div/section/div[4]/ul/li[14]/div/div/div[1]/span[1]/i')
 jiankangmayanse_button.click()
 sleep(1)
+print("\033[0;33m%s\033[0m" % '[56%] Automata processing to Healthcare Code Color part...')  # 33m is yellow
 
-zaixiao_button = driver.find_element_by_xpath\
+zaixiao_button = driver.find_element_by_xpath \
     ('/html/body/div[1]/div[1]/div/section/div[4]/ul/li[15]/div/div/div[1]/span[1]/i')
 zaixiao_button.click()
 sleep(1)
+print("\033[0;33m%s\033[0m" % '[61%] Automata processing to On Campus part...')  # 33m is yellow
 
-diliweizhi_button = driver.find_element_by_xpath\
+diliweizhi_button = driver.find_element_by_xpath \
     ('/html/body/div[1]/div[1]/div/section/div[4]/ul/li[19]/div/input')
 diliweizhi_button.click()
 sleep(1)
+print("\033[0;33m%s\033[0m" % '[66%] Accessing your location...')  # 33m is yellow
 
-jiatingchengyuan_button = driver.find_element_by_xpath\
+jiatingchengyuan_button = driver.find_element_by_xpath \
     ('/html/body/div[1]/div[1]/div/section/div[4]/ul/li[22]/div/div/div[2]/span[1]/i')
 jiatingchengyuan_button.click()
 sleep(1)
+print("\033[0;33m%s\033[0m" % '[82%] Automata processing to Family Member part...')  # 33m is yellow
 
-chengnuo_button = driver.find_element_by_xpath\
+chengnuo_button = driver.find_element_by_xpath \
     ('/html/body/div[1]/div[1]/div/section/div[4]/ul/li[34]/div/div/div/span[1]/i')
 chengnuo_button.click()
 sleep(1)
+print("\033[0;33m%s\033[0m" % '[86%] Automata processing to Integrity Guarantee part...')  # 33m is yellow
 
-submit_button = driver.find_element_by_xpath\
+submit_button = driver.find_element_by_xpath \
     ('/html/body/div[1]/div[1]/div/section/div[5]/div/a')
 submit_button.click()
 sleep(1)
+print("\033[0;32m%s\033[0m" % '[90%] Submitting your report...')  # 32m is green
 
-confirm_button = driver.find_element_by_xpath\
-    ('/html/body/div[4]/div/div[2]/div[2]')
-confirm_button.click()
+try:
+    confirm_button = driver.find_element_by_xpath \
+        ('/html/body/div[4]/div/div[2]/div[2]')
+    confirm_button.click()
+except:
+    print("\033[0;33m%s\033[0m" % "You've already punched the clock today! Please do not punch the clock more than 1 time.")  # 33m is yellow
+    driver.close()
+    exit(0)
+
 sleep(1)
+print("\033[0;32m%s\033[0m" % '[96%] Confirming your status...')  # 32m is green
 
-OK_button = driver.find_element_by_xpath\
+OK_button = driver.find_element_by_xpath \
     ('/html/body/div[1]/div[2]/div/a')
 OK_button.click()
 sleep(2)
+print("\033[0;32m%s\033[0m" % '[100%] Successfully completed the punchclock program!')  # 32m is green
 
 driver.close()
