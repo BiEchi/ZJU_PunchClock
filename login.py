9cdc052033aa544ba3d92b788b4d15bca28f4202
Jackgetup <Haob.19@intl.zju.edu.cn>
2022-10-28 11:01:37 +0800

    updated to latest

--- login.py
+++ login.py
@@ -16,50 +16,37 @@ class login:
         config = configparser.ConfigParser()
         config.read("config.ini")
         # Use scratch
-        # self.account = config['identification_information']['account_config']
-        # self.password = config['identification_information']['password_config']
+        self.account = config['identification_information']['account_config']
+        self.password = config['identification_information']['password_config']
         # Use app
-        self.account = input("Please enter your account (INTL E-mail) e.g. Haob.88@intl.zju.edu.cn\n> ")
-        print("\033[0;32m%s\033[0m" % 'Your account is {}.'.format(self.account)) # 32m is green
-        self.password = input("Please enter your passwd e.g. Popu#7852\n> ")
-        print("\033[0;32m%s\033[0m" % 'Successfully typed in your Password.')  # 32m is green
+        # self.account = input("Please enter your account (INTL E-mail) e.g. Haob.88@intl.zju.edu.cn\n> ")
+        # print("\033[0;32m%s\033[0m" % 'Your account is {}.'.format(self.account)) # 32m is green
+        # self.password = input("Please enter your passwd e.g. Popu#7852\n> ")
+        # print("\033[0;32m%s\033[0m" % 'Successfully typed in your Password.')  # 32m is green
         return
 
     def logging_in_process(self):
         print("\033[0;33m%s\033[0m" % 'Logging you in...')  # 33m is yellow
 
         try:
-            # click the "INTL ID" button
-            self.driver.implicitly_wait(self.timeout)
-            INTL_ID_button_second = self.driver.find_element_by_xpath(
-                '/html/body/div/div[3]/div[2]/div/div[2]/div/div[1]/div/div[1]/a/img')
-            INTL_ID_button_second.click()
-
-
             # enter the account name
-            self.driver.implicitly_wait(self.timeout)
             account_input_box = self.driver.find_element_by_xpath(
-                '/html/body/div/form[1]/div/div/div[2]/div[1]/div/div/div/div/div/div[3]/div/div/div/div[2]/div[2]/div/input[1]')
+                '//*[@id="username"]')
             account_input_box.send_keys(self.account)
             account_input_box.send_keys(Keys.ENTER)
 
             # enter the password and confirm
-
-            self.driver.implicitly_wait(self.timeout)
             password_input_box = self.driver.find_element_by_xpath(
-                '/html/body/div/form[1]/div/div/div[2]/div[1]/div/div/div/div/div/div[3]/div/div[2]/div/div[2]/div/div[2]/input')
+                '//*[@id="password"]')
             password_input_box.send_keys(self.password)
             password_input_box.send_keys(Keys.ENTER)
 
             # click the YES button
-            self.driver.implicitly_wait(self.timeout)
-            YES_button = self.driver.find_element_by_xpath('//*[@id="idSIButton9"]')
+            YES_button = self.driver.find_element_by_xpath('//*[@id="dl"]')
             YES_button.click()
 
         except:
-            print("\033[0;31m%s\033[0m" % 'Wrong account or password!')  # 31m is red
-            self.driver.close()
-            exit(0)
+            print("\033[0;31m%s\033[0m" % 'Something logging wrong but we will resume!')  # 31m is red
 
         return
 
--- main.py
+++ main.py
@@ -5,10 +5,17 @@ from time import sleep
 
 # set the Chrome options
 chrome_options = Options()
-chrome_options.add_argument('--headless')
+# chrome_options.add_argument('--headless')
 
 # use Chrome to access the clock-punching website
 driver = webdriver.Chrome(options=chrome_options)  # omit options if don't want to use headless mode; add options=chrome_options if you want to use headless mode
+params = {
+    # GPS location of Haining
+    "latitude": 30.520463,
+    "longitude": 120.727656,
+    "accuracy": 100
+}
+driver.execute_cdp_cmd("Page.setGeolocationOverride", params)
 driver.get('https://healthreport.zju.edu.cn/ncov/wap/default/index')
 print("\033[0;32m%s\033[0m" % '[10%] Successfully started the automata.')  # 32m is green
 
@@ -27,16 +34,10 @@ sleep(1)
 print("\033[0;33m%s\033[0m" % '[30%] Automata processing to Vaccine part...')  # 33m is yellow
 
 buyi_button = driver.find_element_by_xpath \
-    ('/html/body/div[1]/div[1]/div/section/div[4]/ul/li[5]/div/div/div[5]/span[1]/i')
+    ('/html/body/div[1]/div[1]/div/section/div[4]/ul/li[5]/div/div/div[6]/span[1]/i')
 buyi_button.click()
 sleep(1)
 print("\033[0;33m%s\033[0m" % '[36%] Automata processing to BY part...')  # 33m is yellow
-
-yiwancheng_button = driver.find_element_by_xpath \
-    ('/html/body/div[1]/div[1]/div/section/div[4]/ul/li[6]/div/div/div[2]/span[1]/i')
-yiwancheng_button.click()
-sleep(1)
-print("\033[0;33m%s\033[0m" % '[40%] Automata processing to YWC part...')  # 33m is yellow
 
 fareqingjia_button = driver.find_element_by_xpath \
     ('/html/body/div[1]/div[1]/div/section/div[4]/ul/li[7]/div/div/div[2]/span[1]/i')
@@ -50,39 +51,19 @@ farewai_button.click()
 sleep(1)
 print("\033[0;33m%s\033[0m" % '[47%] Automata processing to FW part...')  # 33m is yellow
 
-jiankangma_button = driver.find_element_by_xpath \
-    ('/html/body/div[1]/div[1]/div/section/div[4]/ul/li[13]/div/div/div[1]/span[1]/i')
-jiankangma_button.click()
+shixishijian_button = driver.find_element_by_xpath \
+    ('/html/body/div[1]/div[1]/div/section/div[4]/ul/li[8]/div/div/div[3]/span[1]/i')
+shixishijian_button.click()
 sleep(1)
-print("\033[0;33m%s\033[0m" % '[52%] Automata processing to Healthcare Code part...')  # 33m is yellow
-
-# will show up after clicking "jiankangma" button
-jiankangmayanse_button = driver.find_element_by_xpath \
-    ('/html/body/div[1]/div[1]/div/section/div[4]/ul/li[14]/div/div/div[1]/span[1]/i')
-jiankangmayanse_button.click()
-sleep(1)
-print("\033[0;33m%s\033[0m" % '[56%] Automata processing to Healthcare Code Color part...')  # 33m is yellow
-
-zaixiao_button = driver.find_element_by_xpath \
-    ('/html/body/div[1]/div[1]/div/section/div[4]/ul/li[15]/div/div/div[1]/span[1]/i')
-zaixiao_button.click()
-sleep(1)
-print("\033[0;33m%s\033[0m" % '[61%] Automata processing to On Campus part...')  # 33m is yellow
 
 diliweizhi_button = driver.find_element_by_xpath \
-    ('/html/body/div[1]/div[1]/div/section/div[4]/ul/li[19]/div/input')
+    ('/html/body/div[1]/div[1]/div/section/div[4]/ul/li[11]/div/input')
 diliweizhi_button.click()
 sleep(1)
 print("\033[0;33m%s\033[0m" % '[66%] Accessing your location...')  # 33m is yellow
 
-jiatingchengyuan_button = driver.find_element_by_xpath \
-    ('/html/body/div[1]/div[1]/div/section/div[4]/ul/li[22]/div/div/div[2]/span[1]/i')
-jiatingchengyuan_button.click()
-sleep(1)
-print("\033[0;33m%s\033[0m" % '[82%] Automata processing to Family Member part...')  # 33m is yellow
-
 chengnuo_button = driver.find_element_by_xpath \
-    ('/html/body/div[1]/div[1]/div/section/div[4]/ul/li[34]/div/div/div/span[1]/i')
+    ('/html/body/div[1]/div[1]/div/section/div[4]/ul/li[28]/div/div/div/span[1]/i')
 chengnuo_button.click()
 sleep(1)
 print("\033[0;33m%s\033[0m" % '[86%] Automata processing to Integrity Guarantee part...')  # 33m is yellow
